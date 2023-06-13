d5c2e5799b4ea4ceedfd341faef73d32be118eef
purplegrape <purplegrape4@gmail.com>
2023-06-13 13:45:33 +0800

    update

--- web/datamodels/2.x/itop-config-mgmt/dictionaries/zh_cn.dict.itop-config-mgmt.php
+++ web/datamodels/2.x/itop-config-mgmt/dictionaries/zh_cn.dict.itop-config-mgmt.php
@@ -1302,7 +1302,7 @@ Dict::Add('ZH CN', 'Chinese', '简体中文', array(
 
 	'Class:IPInterface/Attribute:macaddress' => 'MAC 地址',
 	'Class:IPInterface/Attribute:macaddress+' => '',
-	'Class:IPInterface/Attribute:comment' => '注释',
+	'Class:IPInterface/Attribute:comment' => '备注',
 	'Class:IPInterface/Attribute:coment+' => '',
 	'Class:IPInterface/Attribute:ipgateway' => '网关',
 	'Class:IPInterface/Attribute:ipgateway+' => '',
