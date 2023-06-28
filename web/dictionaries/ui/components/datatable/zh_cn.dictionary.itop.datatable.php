07f6053e8ed0c020f3f04bc6d4980a5405f429de
purplegrape <purplegrape4@gmail.com>
2023-06-28 13:37:31 +0800

    update

--- web/dictionaries/ui/components/datatable/zh_cn.dictionary.itop.datatable.php
+++ web/dictionaries/ui/components/datatable/zh_cn.dictionary.itop.datatable.php
@@ -20,7 +20,7 @@
 // Display DataTable
 Dict::Add('ZH CN', 'Chinese', '简体中文', array(
 	'UI:Datatables:Language:Processing'        => '请稍候...',
-	'UI:Datatables:Language:LengthMenu'        => '每页 _MENU_ ',
+	'UI:Datatables:Language:LengthMenu'        => '每页 _MENU_ 项',
 	'UI:Datatables:Language:ZeroRecords'       => '未找到相关结果',
 	'UI:Datatables:Language:Info'              => '共 _TOTAL_ 项',
 	'UI:Datatables:Language:InfoEmpty'         => '未找到相关信息',
